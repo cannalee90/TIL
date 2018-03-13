b826655753ae5d5ba915390031fcd21ad5226d89
Kangho Lee <cannalee90@gmail.com>
2018-03-13 22:40:37 +0900

    merged

--- problem_solving/1476_1.cpp
+++ problem_solving/1476_1.cpp
@@ -2,31 +2,28 @@
 // Created by  이강호 on 2018. 3. 11..
 //
 // 중국인의 나머지 정리를 이용해서 문제를 풀 수 있다.
-// http://joonas.tistory.com/23 이곳을 참조해서 문제를 풀 었다.
-// (28 * 19) ^ a1 mod 15 = 1
-// (15 * 19) ^ a2 mod 28 = 1
-// (15 * 28) ^ a3 mod 19 = 1
-// 각각의 a1, a2, a3의 값을 구해놓고 문제를 풀면 간단하게 풀 수 있다.
-
+// http://joonas.tistory.com/23 이 두곳을 참조해서
+// https://namu.wiki/w/%EC%A4%91%EA%B5%AD%EC%9D%B8%EC%9D%98%20%EB%82%98%EB%A8%B8%EC%A7%80%20%EC%A0%95%EB%A6%AC#fn-10
+// https://www.youtube.com/watch?v=ru7mWZJlRQg
+//
+// (28 * 19 * e) % 15 == (532 * e) % 15 == (7 * er) % 15 == 1
+// (15 * 19 * s) % 28 == (285 * s) % 28 == (5 * sr) % 28 == 1
+// (15 * 28 * m) % 19 == (420 * m) % 19 == (2 * mr) % 19 == 1
+//
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
-  int e, s, m;
-  int emax = 15, smax = 28, mmax = 19;
-  long long tmp = 28 * 19;
-  for(int i = 1; ; i++) {
-    cout << tmp << endl;
-    if(tmp % emax == 1) {
-      cout << i << endl;
-      cout << tmp << endl;
-      break;
-    }
-    tmp *= 28 * 19;
+  long long e, s, m;
+  long long er = 13, sr = 17, mr = 10;
+  long long emax = 15, smax = 28, mmax = 19;
+  cin >> e >> s >> m;
+  long long ans = ((e * 6916) + (4845 * s) + (m * 4200));
+  if (!(ans % 7980)) {
+    cout << "7980" << endl;
+  } else {
+    cout << ans % 7980 << endl;
   }
-//  cin >> e >> s >> m;
 }
-
-
