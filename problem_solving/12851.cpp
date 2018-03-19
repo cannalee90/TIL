bf7ad5fb9b092bcbbb26c863b254b2d1dabb5498
Kangho Lee <cannalee90@gmail.com>
2018-03-19 14:01:13 +0900

    bfs
    내 방식이 틀린지 알고 새로운 방식의 bfs를 사용해보았는데.. 알고보니까 edge가 문제였던 것이었다.....ㅜㅜ
    
    졸지 말고 문제풀자..

---
+++ problem_solving/12851.cpp
@@ -0,0 +1,75 @@
+//
+// Created by 강호  이 on 2018. 3. 19..
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stdio.h>
+#include <algorithm>
+#include <tuple>
+#include <numeric>
+
+using namespace std;
+
+#define INF 987654321
+
+int mmove[2] = {1, -1};
+int s, e;
+
+bool isRange(int c) {
+  if (c > 100005 || c < 0) return false;
+  return true;
+}
+
+int main() {
+  int ans = 0;
+  int dis = 0;
+  queue<int> q;
+  cin >> s >> e;
+  vector<int> visited(100002, INF);
+
+  q.push(s);
+  visited[s] = dis;
+  if(s == e) {
+    cout << '0' << endl;
+    cout << '1' << endl;
+    exit(1);
+  }
+  while (q.size()) {
+    dis++;
+    int length = q.size();
+    for(int k = 0; k < length; k++) {
+      int c = q.front();
+      q.pop();
+      for(int i = 0; i < 2; i++) {
+        int nextC = c + mmove[i];
+        if(isRange(nextC) && visited[nextC] >= dis) {
+          visited[nextC] = dis;
+          q.push(nextC);
+          if(nextC == e) {
+            ans ++;
+          }
+
+        }
+      }
+
+      int nextC = c * 2;
+      if (isRange(nextC) && visited[nextC] >= dis) {
+        visited[nextC] = dis;
+        q.push(nextC);
+        if(nextC == e) {
+          ans ++;
+        }
+
+      }
+
+    }
+    if(ans) {
+      break;
+    }
+
+  }
+  cout << dis << endl;
+  cout << ans << endl;
+}
